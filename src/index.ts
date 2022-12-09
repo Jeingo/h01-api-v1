c904217c0e3f5c3a7070befc572f810983487204
Renat <jeingo20@gmail.com>
2022-12-09 20:43:42 +0300

    add router videos

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import express from 'express'
+import {videosRouter} from "./routers/videos-router";
 
 const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
-})
+app.use('/videos', videosRouter)
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`)
---
+++ src/routers/videos-router.ts
@@ -0,0 +1,23 @@
+import {Router} from 'express'
+
+export const videosRouter = Router({})
+
+videosRouter.get('/', (req, res) => {
+
+})
+
+videosRouter.post('/', (req, res) => {
+
+})
+
+videosRouter.get('/:id', (req, res) => {
+
+})
+
+videosRouter.put('/:id', (req, res) => {
+
+})
+
+videosRouter.delete('/:id', (req, res) => {
+
+})
