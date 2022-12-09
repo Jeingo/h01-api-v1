ce16c606e6b3872f5abd0003f9c249da9320d9ab
Renat <jeingo20@gmail.com>
2022-12-09 20:34:27 +0300

    I checked that everything works

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 import express from 'express'
 
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello!')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
+
